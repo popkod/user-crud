34373f118c0098293bb2ca588925866c4d107a95
Endre Varga <drupapi@popkod.hu>
2017-03-03 11:52:38 +0100

    prepare for tests

--- src/UserCrud/Controllers/UserController.php
+++ src/UserCrud/Controllers/UserController.php
@@ -2,8 +2,6 @@
 
 namespace PopCode\UserCrud\Controllers;
 
-use Config;
-use Request;
 use Illuminate\Routing\Controller as BaseController;
 use PopCode\UserCrud\Interfaces\UserControllerInterface;
 
@@ -12,14 +10,16 @@ class UserController extends BaseController implements UserControllerInterface
 
     protected $model;
 
-    public function __construct() {
-        $this->model = Config::get('popcode-usercrud.model', \PopCode\UserCrud\Models\User::class);
+    public function __construct($model = null) {
+        if ($model) {
+            $this->model = $model;
+        } else {
+            $this->model = \Config::get('popcode-usercrud.model', \PopCode\UserCrud\Models\User::class);
+        }
     }
 
     public function index() {
-        /* @var \Illuminate\Database\Eloquent\Model|\PopCode\UserCrud\Models\User $model */
-        $model = new $this->model;
-        $users = $model->all();
+        $users = $this->indexUsers();
 
         return $this->responseGenerator($users, 'index');
     }
@@ -29,68 +29,112 @@ class UserController extends BaseController implements UserControllerInterface
     }
 
     public function store() {
-        $data = Request::all();
+        $data = \Request::all();
 
-        /* @var \Illuminate\Database\Eloquent\Model|\PopCode\UserCrud\Models\User $model */
-        $model = new $this->model;
-        $validator = $model->validate($data);
-
-        if ($validator->fails()) {
-            return $this->errorResponseGenerator($data, $validator->messages(), 'store');
+        $errors = $this->hasError($data, 'store');
+        if ($errors) {
+            return $this->errorResponseGenerator($data, $errors, 'store');
         }
 
-        $user = new $this->model;
-        $user->fill($data)
-            ->save();
+        $user = $this->storeUser($data);
 
         return $this->responseGenerator($user, 'store');
     }
 
     public function show($id) {
-        /* @var \Illuminate\Database\Eloquent\Model|\PopCode\UserCrud\Models\User $model */
-        $model = new $this->model;
-        $user = $model->find($id);
+        $user = $this->showUser($id);
 
         return $this->responseGenerator($user, 'show');
     }
 
     public function edit($id) {
-        /* @var \Illuminate\Database\Eloquent\Model|\PopCode\UserCrud\Models\User $model */
-        $model = new $this->model;
-        $user = $model->find($id);
+        $user = $this->showUser($id);
 
         return $this->responseGenerator($user, 'show');
     }
 
     public function update($id) {
-        $data = Request::all();
+        $data = \Request::all();
 
-        /* @var \Illuminate\Database\Eloquent\Model|\PopCode\UserCrud\Models\User $model */
-        $model = new $this->model;
-        $validator = $model->validate($data);
-
-        if ($validator->fails()) {
-            return $this->errorResponseGenerator($data, $validator->messages(), 'store');
+        $errors = $this->hasError($data, 'store');
+        if ($errors) {
+            return $this->errorResponseGenerator($data, $errors, 'store');
         }
 
-        $user = $model->find($id);
-        $user->fill($data)
-            ->save();
+        $user = $this->updateUser($id, $data);
 
         return $this->responseGenerator($user, 'update');
     }
 
     public function destroy($id) {
-        /* @var \Illuminate\Database\Eloquent\Model|\PopCode\UserCrud\Models\User $model */
-        $model = new $this->model;
-        $user = $model->find($id);
-        $user->email .= '_deleted';
-        $user->delete();
+        $succeed = $this->destroyUser($id);
+
+        if ($succeed) {
+            return $this->responseGenerator([], 'destroy');
+        }
+        return $this->errorResponseGenerator(['id' => $id], [], 'destroy');
     }
 
 
+
+    protected function indexUsers() {
+        /* @var \Illuminate\Database\Eloquent\Model|\PopCode\UserCrud\Models\User $model */
+        $model = new $this->model;
+        return $model->all();
+    }
+
+    protected function showUser($id) {
+        /* @var \Illuminate\Database\Eloquent\Model|\PopCode\UserCrud\Models\User $model */
+        $model = new $this->model;
+        $user = $model->find($id);
+        return $user;
+    }
+
+    protected function storeUser($userData) {
+        /* @var \PopCode\UserCrud\Models\User $user */
+        $user = new $this->model;
+        $user->fill($userData);
+        $user->save();
+
+        return $user;
+    }
+
+    protected function updateUser($id, $userData) {
+        /* @var \PopCode\UserCrud\Models\User $user */
+        $user = new $this->model;
+        $user = $user->find($id);
+
+        $user->fill($userData);
+        $user->save();
+
+        return $user;
+    }
+
+    protected function destroyUser($id) {
+        /* @var \Illuminate\Database\Eloquent\Model|\PopCode\UserCrud\Models\User $model */
+        $model = new $this->model;
+        /* @var \PopCode\UserCrud\Models\User $user */
+        $user = $model->find($id);
+        return $user->delete() === true;
+    }
+
+
+    protected function hasError($userData, $type = null) {
+        /* @var \Illuminate\Database\Eloquent\Model|\PopCode\UserCrud\Models\User $model */
+        $model = new $this->model;
+        $validator = $model->validate($userData, $type);
+
+        if ($validator->fails()) {
+            return $validator->messages();
+        }
+
+        return false;
+    }
+
+
+
     protected function responseGenerator($responseData, $type = null) {
-        if (Request::ajax() || Request::wantsJson()) {
+        if (\Request::ajax() || \Request::wantsJson()) {
             return response()->json($responseData);
         }
 
@@ -99,7 +143,7 @@ class UserController extends BaseController implements UserControllerInterface
     }
 
     protected function errorResponseGenerator($data, $messages, $type = null, $status = 400) {
-        if (Request::ajax() || Request::wantsJson()) {
+        if (\Request::ajax() || \Request::wantsJson()) {
             return response()->json(['error' => $messages], $status);
         }
 
