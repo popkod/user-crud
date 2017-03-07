cda819d76f39a2784acb5cd89fcd28e07fac2263
Endre Varga <drupapi@popkod.hu>
2017-03-07 18:27:35 +0100

    route fix

--- routes/popcode-usercrud-routes.php
+++ routes/popcode-usercrud-routes.php
@@ -1,5 +1,11 @@
 <?php
 
-Illuminate\Support\Facades\Route::get('/api/users/roles', Config::get('popcode-usercrud.role_controller', [Popcode\UserCrud\Controllers\RoleController::class, 'index']));
+Illuminate\Support\Facades\Route::get(
+    '/api/users/roles',
+    Config::get('popcode-usercrud.role_controller', Popcode\UserCrud\Controllers\RoleController::class) . '@index'
+);
 
-Illuminate\Support\Facades\Route::resource('/api/users', Config::get('popcode-usercrud.controller', Popcode\UserCrud\Controllers\UserController::class));
+Illuminate\Support\Facades\Route::resource(
+    '/api/users',
+    Config::get('popcode-usercrud.controller', Popcode\UserCrud\Controllers\UserController::class)
+);
